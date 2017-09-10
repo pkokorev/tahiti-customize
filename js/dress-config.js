decf65a5f25e068a7f423cc3f78217d4f08d800b
Petr KOKOREV <pkokorev@iterial.ch>
2017-09-10 18:38:53 +0200

    Cockpit CMS integration

--- js/dress-config.js
+++ js/dress-config.js
@@ -1,4 +1,4 @@
-(function (ng, papa) {
+(function (ng) {
     'use strict';
 
     function calculateTotalPrice(db, criteria, log) {
@@ -51,21 +51,61 @@
         }
     }
 
+    function preLoadImages(images) {
+        function handleImage(event) {
+            event.target.remove();
+        }
+
+        ng.forEach(images, function (image) {
+            var newImage = new Image();
+
+            newImage.onload = handleImage;
+            newImage.onerror = handleImage;
+            newImage.src = image;
+        });
+    }
+
     ng.module('DressConfigApp', ['ngAnimate'])
-        .factory('$dataFactory', ['$timeout', function (timeout) {
+        .constant('COCKPIT_ROOT', 'https://brandizi.fr/dress/cockpit')
+        .config(['$sceDelegateProvider', 'COCKPIT_ROOT', function (sceDelegateProvider, root) {
+            sceDelegateProvider.resourceUrlWhitelist(['self', root + '/**']);
+        }])
+        .factory('$dataFactory', ['$timeout', '$http', '$log', 'COCKPIT_ROOT', function (timeout, http, log, root) {
+            var cockpitApiRoot = root + '/api',
+                token = '3c986fd55b6bace75ceba33d78b019';
             var db = {};
             return {
                 loadData: function (name, callback) {
-                    papa.parse('data/' + name + '.csv', {
-                        download: true,
-                        header: true,
-                        dynamicTyping: true,
-                        complete: function (results) {
-                            db[name] = mapById(results.data, 'id');
-                            timeout(function () {
-                                callback(results);
-                            });
+                    http({
+                        method: 'GET',
+                        url: cockpitApiRoot + '/collections/get/' + name,
+                        params: {
+                            token: token
                         }
+                    }).then(function (result) {
+                        if (ng.isObject(result.data) && ng.isArray(result.data.entries)) {
+                            db[name] = mapById(result.data.entries, 'id');
+                            timeout(function () {
+                                callback(result.data.entries);
+                            });
+                        } else {
+                            log.error('Invalid response object structure', result);
+                        }
+                    }, function (error) {
+                        log.error('Error loading collection : ' + name, error);
+                    });
+                },
+                loadFragment: function (name, callback) {
+                    http({
+                        method: 'GET',
+                        url: cockpitApiRoot + '/regions/data/' + name,
+                        params: {
+                            token: token
+                        }
+                    }).then(function (result) {
+                        callback(result.data);
+                    }, function (error) {
+                        log.error('Error loading region : ' + name, error);
                     });
                 },
                 db: function () {
@@ -73,14 +113,12 @@
                 }
             };
         }])
-        .controller('DressConfigCtrl', ['$scope', '$dataFactory', '$log', function (scope, dataFactory, log) {
+        .controller('DressConfigCtrl', ['$scope', '$dataFactory', '$sce', '$log', function (scope, dataFactory, sce, log) {
             scope.criteria = {
                 sizeId: '2_medium',
-                tissueId: '3_t',
-                laceId: '5_l',
-                flowerId: '6_f',
-                deliveryId: '4_d',
-                crafted: 'true'
+                deliveryId: '1_d',
+                crafted: 'true',
+                loaded: false
             };
             scope.total = {
                 scope: 0,
@@ -88,6 +126,7 @@
             };
             scope.lists = {
                 sizes: [],
+                models: [],
                 tissues: [],
                 laces: [],
                 flowers: [],
@@ -96,24 +135,34 @@
             scope.toggle = {
                 sizesDescription: false
             };
-            scope.description =
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque iaculis leo feugiat sem porta ' +
-                'malesuada non sed urna. Phasellus sapien libero, varius vestibulum libero id, condimentum ' +
-                'pellentesque felis. Nullam lobortis urna augue, eu fringilla erat efficitur ultrices.';
+            scope.description = {
+                html: '',
+                getHtml: function () {
+                    return sce.trustAsHtml(scope.description.html);
+                }
+            };
 
             (function (dataLists, loadedLists) {
-                dataLists.forEach(function (dataList) {
+                ng.forEach(dataLists, function (dataList) {
                     dataFactory.loadData(dataList, function (results) {
-                        copyArray(results.data, scope.lists[dataList]);
+                        copyArray(results, scope.lists[dataList]);
                         loadedLists.push(dataList);
-                        if (dataLists.length === loadedLists.length) {
+                        if (Object.keys(dataLists).length === loadedLists.length) {
                             scope.$watch('criteria', function (value) {
                                 scope.total.amount = calculateTotalPrice(dataFactory.db(), value || {}, log).total;
                             }, true);
+                            scope.criteria.loaded = true;
                         }
                     });
                 });
-            })(['sizes', 'tissues', 'laces', 'flowers', 'deliveries'], []);
+            })(['sizes', 'models', 'tissues', 'laces', 'flowers', 'deliveries'], []);
+            (function (documentFragments) {
+                ng.forEach(documentFragments, function (documentFragment) {
+                    dataFactory.loadFragment(documentFragment, function (data) {
+                        scope.description.html = data.description;
+                    });
+                });
+            })(['configuration']);
         }])
         .directive('tahitiCaption', [function () {
             return {
@@ -135,7 +184,8 @@
                 }
             };
         }])
-        .directive('tahitiRadioGroup', [function () {
+        .directive('tahitiRadioGroup', ['$sce', 'COCKPIT_ROOT', function (sce, root) {
+            var cockpitUploadsRoot = root + '/storage/uploads';
             return {
                 restrict: 'E',
                 require: 'ngModel',
@@ -153,13 +203,15 @@
                 },
                 templateUrl: '/tahiti-radio-group.html',
                 link: function (scope, element, attrs, ngModel) {
-                    window.console.log('Toggle', scope.toggleObject, scope.toggleKey);
                     scope.local = {
                         items: scope.items,
                         selectedId: scope.ngModel,
                         desktopLayout: scope.desktopLayout === 'horizontal' ? scope.desktopLayout : 'vertical',
                         mobileLayout: scope.mobileLayout === 'vertical' ? scope.mobileLayout : 'horizontal',
                         tiles: !!scope.tiles
+                    };
+                    scope.imageUrl = function (path) {
+                        return sce.getTrustedResourceUrl(cockpitUploadsRoot + path);
                     };
                     scope.handleChange = function (selectedId) {
                         scope.local.selectedId = selectedId;
@@ -217,41 +269,70 @@
                 }
             };
         }])
-        .directive('tahitiDressSlider', [function () {
+        .directive('tahitiDressSlider', ['$sce', 'COCKPIT_ROOT', function (sce, root) {
+            var cockpitUploadsRoot = root + '/storage/uploads';
             return {
                 restrict: 'E',
                 scope: {
                     criteria: '=',
                     initialVariant: '@',
-                    tissues: '='
+                    tissues: '=',
+                    sizes: '=',
+                    models: '='
                 },
                 templateUrl: '/tahiti-dress-slider.html',
                 link: function (scope) {
 
                     function calculateShape(sizeId) {
-                        if (['1_small', '2_medium'].indexOf(sizeId) >= 0) {
-                            return 'skinny';
-                        } else {
-                            return 'round';
-                        }
+                        var sizes = mapById(scope.sizes, 'id'),
+                            models = mapById(scope.models, 'id'),
+                            size = sizes[sizeId],
+                            model = size ? size.model : null,
+                            modelId = model ? model.display : null;
+                        return models[modelId];
                     }
 
-                    var shape = calculateShape(scope.criteria.sizeId),
+                    var model = null,
                         variant = scope.initialVariant;
 
-                    scope.getShape = function () {
-                        return shape;
-                    };
                     scope.getVariant = function () {
                         return variant;
                     };
                     scope.rotate = function () {
                         variant = variant === 'front' ? 'back' : 'front';
                     };
+                    scope.modelImageUrl = function () {
+                        if (model) {
+                            var dressed = !!scope.criteria.tissueId ? 'dressed' : 'naked',
+                                imageKey = variant + '-' + dressed;
+                            return sce.getTrustedResourceUrl(cockpitUploadsRoot + model[imageKey].path);
+                        } else {
+                            return null;
+                        }
+                    };
+                    scope.canvasImageUrl = function (path) {
+                        return sce.getTrustedResourceUrl(cockpitUploadsRoot + path);
+                    };
 
                     scope.$watch('criteria', function (criteria) {
-                        shape = calculateShape(criteria.sizeId);
+                        model = calculateShape(criteria.sizeId);
                     }, true);
+                    scope.$watch('models', function (models) {
+                        var images = [];
+                        ng.forEach(models, function (model) {
+                            ng.forEach(['front-dressed', 'front-naked', 'back-dressed', 'back-naked'], function (key) {
+                                images.push(cockpitUploadsRoot + model[key].path);
+                            });
+                        });
+                        preLoadImages(images);
+                    }, true);
+                    scope.$watch('tissues', function (tissues) {
+                        var images = [];
+                        ng.forEach(tissues, function (tissue) {
+                            images.push(cockpitUploadsRoot + tissue.canvas.path);
+                        });
+                        preLoadImages(images);
+                    });
                 }
             };
         }])
@@ -273,27 +354,6 @@
                     }, true);
                 }
             };
-        }])
-        .directive('initialLoader', [function () {
-            return {
-                restrict: 'E',
-                scope: {},
-                link: function (scope, element, attrs) {
-                    ng.forEach(attrs, function (value, key) {
-                        if (key.indexOf('preloadImg') >= 0) {
-                            var newImage = new Image();
-
-                            newImage.onload = handle;
-                            newImage.onerror = handle;
-                            newImage.src = value;
-                        }
-                    });
-
-                    function handle() {
-                        event.target.remove();
-                    }
-                }
-            };
         }]);
 
-})(angular, Papa);
+})(angular);
